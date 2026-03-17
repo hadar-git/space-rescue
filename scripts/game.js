aa378d9e7d1f5e5c1b52146276ac51ab4494f388
hadar <hadar@kazoula.com>
2026-03-17 23:00:03 +0200

    add start game func and  input func(collector)

--- scripts/game.js
+++ scripts/game.js
@@ -32,3 +32,35 @@ const initPage = () => {
     // הצגת מודאל התחלה
     document.getElementById('startModal').style.display = 'flex';
 };
+
+
+const startGame = () => {
+    // הסתרת כל המודאלים בצורה נקייה
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => modal.style.display = 'none');
+    
+    gameState.isGameActive = true;
+    gameState.secretCode = randomSecretCode(gameState.difficulty);
+    console.log("Secret Code (Dev):", gameState.secretCode);
+    
+    renderButtons();
+    startTimer();
+};
+
+const renderButtons = () => {
+    const inputArea = document.getElementById('inputArea');
+    
+   
+    while (inputArea.firstChild) {
+        inputArea.removeChild(inputArea.firstChild);
+    }
+
+    for (let i = 0; i < 10; i++) {
+        const btn = document.createElement('button');
+        btn.textContent = i; 
+        btn.classList.add('num-btn');
+        btn.addEventListener('click', () => handleInput(i));
+        inputArea.appendChild(btn);
+    }
+};
+
